a0554de201efd5c36997552b55fdd518b8ef210c
Александр Романов <roman.ulbricht@yandex.ru>
2022-08-18 16:12:47 +0300

    konec

--- yatube/about/views.py
+++ yatube/about/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import render
 from django.views.generic.base import TemplateView
 
 # Create your views here.
+
+
 class AboutAuthorView(TemplateView):
     template_name = 'about/author.html'
 
